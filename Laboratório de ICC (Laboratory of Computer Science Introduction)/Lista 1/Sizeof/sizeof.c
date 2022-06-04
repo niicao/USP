c2f303a688be3d64c97d0836a4c692d67005a4b1
Nicholas Ikai <83601712+niicao@users.noreply.github.com>
2022-06-03 22:22:57 -0300

    Create sizeof.c

---
+++ Laboratório de ICC (Laboratory of Computer Science Introduction)/Lista 1/Sizeof/sizeof.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%lu\n", sizeof(unsigned char));
+    printf("%lu\n", sizeof(char));
+    printf("%lu\n", sizeof(unsigned int));
+    printf("%lu\n", sizeof(short int));
+    printf("%lu\n", sizeof(int));
+    printf("%lu\n", sizeof(long int));
+    printf("%lu\n", sizeof(unsigned long int));
+    printf("%lu\n", sizeof(long long int));
+    printf("%lu\n", sizeof(float));
+    printf("%lu\n", sizeof(double));
+    printf("%lu\n", sizeof(long double));
+    return 0;
+}
