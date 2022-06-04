bf08d4f011d3149e300c71a45e22d3e5168df516
Nicholas Ikai <83601712+niicao@users.noreply.github.com>
2022-06-03 22:02:43 -0300

    Create Area_do_Quadrado.c

---
+++ ICC/Lista 1/Area_do_Quadrado.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int lado_do_quadrado;
+    int area_do_quadrado;
+    scanf("%d",&lado_do_quadrado);
+    area_do_quadrado = lado_do_quadrado * lado_do_quadrado;
+    printf("%d",area_do_quadrado);
+
+    return 0;
+}
