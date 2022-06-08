f3fbf343eb016be5c5e40fd2ab261fda6276bee4
Nicholas Ikai <83601712+niicao@users.noreply.github.com>
2022-06-08 18:20:15 -0300

    Create translacao_da_reta.c

---
+++ ICC/Lista 2/Translação da reta/translacao_da_reta.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    float result
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    result = (-(b + c));
+    result = result/a;
+    printf("%.2f", result);
+    return 0;
+}
